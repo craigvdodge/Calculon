bbbe8a4241f2aeca6f8ed71e49799aac74ea0c89
craigvdodge <craigvdodge@gmail.com>
2020-12-28 19:42:51 -0800

    Number Comparisson Extension functions.

---
+++ lib/Comparison.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Numerics;
+
+namespace Calculon.Types
+{
+    public static class ComparisonExtensions
+    {
+        public static bool Equals(this Number lhs, Number rhs)
+        {
+            return (lhs.Numerator == rhs.Numerator && lhs.Denominator == rhs.Denominator);
+        }
+
+        public static bool Equals(this Number lhs, int rhs)
+        {
+            Number newRhs = new Number(new BigInteger(rhs));
+            return Equals(lhs, newRhs);
+        }
+
+        public static bool LessThan(this Number lhs, Number rhs)
+        {
+            BigInteger newDenom = lhs.Denominator.LCM(rhs.Denominator);
+            BigInteger newLhsNum = lhs.Numerator * (newDenom / lhs.Denominator);
+            BigInteger newRhsNum = rhs.Numerator * (newDenom / rhs.Denominator);
+            return newLhsNum < newRhsNum;
+        }
+
+        public static bool LessThan(this Number lhs, int rhs)
+        {
+            Number newRhs = new Number(new BigInteger(rhs));
+            return LessThan(lhs, newRhs);
+        }
+
+        public static bool GreaterThan(this Number lhs, Number rhs)
+        {
+            BigInteger newDenom = lhs.Denominator.LCM(rhs.Denominator);
+            BigInteger newLhsNum = lhs.Numerator * (newDenom / lhs.Denominator);
+            BigInteger newRhsNum = rhs.Numerator * (newDenom / rhs.Denominator);
+            return newLhsNum > newRhsNum;
+        }
+
+        public static bool GreaterThan(this Number lhs, int rhs)
+        {
+            Number newRhs = new Number(new BigInteger(rhs));
+            return GreaterThan(lhs, newRhs);
+        }
+
+    }
+}
